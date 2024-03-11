3ed132261225fd7f83a7992a038e61cac6e44056
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-03-11 07:36:51 +0000

    add cache first strategy

--- serviceworker.js
+++ serviceworker.js
@@ -8,10 +8,33 @@ const assets = [
 	'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
 ];
 
+// Service Worker Installation
 // 'self' used instead of 'this', it's safer to use
 // self is the service worker itself
-self.addEventListener('install', function(event) {
-    caches.open('assets').then(function(cache) {
-      return cache.addAll(assets);
-    })
+self.addEventListener("install", event => {
+    // install the assets of my PWA
+    event.waitUntil(
+        caches.open("assets").then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+    // self.skipWaiting(); // activate the service worker immediately
+});
+
+
+// Cache first strategy
+self.addEventListener("fetch", event => {
+  // respondWith accepts both a promise or a response
+    event.respondWith(
+        caches.match(event.request)  // searching in the cache
+            .then( response => {
+                if (response) {
+                    // The request is in the cache 
+                    return response; // cache hit
+                } else {
+                    // We need to go to the network  
+                    return fetch(event.request);  // cache miss
+                }
+            })
+    );
 });
